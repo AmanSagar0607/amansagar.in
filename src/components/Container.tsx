f9eae172dcc87dbbaa8cc9b9f0c9999c7ee79c49
Aman Sagar <amansagar0307@gmail.com>
2025-05-26 00:53:22 +0530

    first commit

---
+++ src/components/Container.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { cn } from '@/lib/utils'
+
+export const Containers = ({children, className}: {children: React.ReactNode, className?: string}) => {
+  return (
+    <div className={cn("max-w-4xl w-full mx-auto bg-white dark:bg-black", className)}> {children}
+    </div>
+  )
+}
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,91 @@
+
+
+"use client";
+
+import { useState } from "react";
+import { Containers } from "@/components/Container";
+import Image from "next/image";
+import Link from "next/link";
+import { motion, useMotionValueEvent, useScroll } from "motion/react";
+
+
+import React from 'react'
+
+export const Navbar = () => {
+    const navItems = [
+        {
+            title: "About",
+            href: "/about",
+        },
+        {
+            title: "Projects",
+            href: "/projects",
+        },
+        {
+            title: "Contact",
+            href: "/contact",
+        },
+        {
+            title:"Blog",
+            href:"/blog",
+        },
+        {
+            title:"Resume",
+            href:"/resume",
+        }
+    ];
+
+    const [hovered, setHovered] = useState<number | null>(null);
+    const {scrollY} = useScroll();
+
+    const [scrolled, setScrolled] = useState<boolean>(false);
+
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        console.log("scrollY",latest);
+        if (latest > 20) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    });
+
+  return (
+    <Containers>
+        <motion.nav 
+       animate={{
+        boxShadow: scrolled ? "var(--shadow-acceternity)" : "none",
+        width: scrolled ? "45%" : "100%",
+        y: scrolled ? "10" : "0",
+         }}
+         transition={{
+            duration: 0.3,
+            ease: "easeInOut",
+         }}
+        className={`fixed inset-x-0 top-4 z-50 mx-auto flex max-w-4xl items-center justify-between p-1 px-3 rounded-full  ${scrolled ? "shadow-acceternity" : ""}`}>
+            {/* Avatar */}
+            <Image className="h-12 w-12 rounded-full" src="/avatar.webp" alt="Avatar" width={100} height={100} />
+           
+            {/* NavItems */}
+            <div className="flex  items-center gap-4">
+                {navItems.map((item, idx) => (
+                    <Link 
+                         className="text-sm relative px-2 py-1 md:text-base text-secondary"
+                         key={idx}
+                         href={item.href}
+                         onMouseEnter={() => setHovered(idx)}
+                         onMouseLeave={() => setHovered(null)}>
+                            {hovered === idx && (
+                            <motion.span layoutId="hovered-span" className="h-full w-full absolute inset-0 rounded-md  bg-neutral-100 dark:bg-neutral-800" 
+                            initial={{ width: 0 }}
+                            animate={{ width: "100%" }}
+                            transition={{ duration: 0.3 }}
+                            />
+                         )}
+                        <span className=" relative z-10">{item.title}</span>
+                     </Link>
+                ))}
+            </div>
+        </motion.nav>
+    </Containers>
+  )
+}
