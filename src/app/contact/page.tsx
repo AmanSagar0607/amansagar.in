d7852c22e5f39f4c748e10d09c794943deaa6dbd
Aman Sagar <amansagar0307@gmail.com>
2025-07-04 23:09:43 +0530

    lalalala...

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,18 +1,18 @@
 import { Container } from "@/components/Container";
 import { Heading } from "@/components/heading";
 import { SubHeading } from "@/components/subheading";
-import { ContactForm } from "@/components/contact-form";
+import { TallyForm } from "@/components/tally-form";
 
 export default function ContactPage() {
   return (
     <div className={`flex min-h-screen items-start justify-center`}>
-      <Container className="min-h-[200vh] p-4 px-8 pt-24 pb-10 md:px-12 md:pt-28 md:pb-10">
+      <Container className="min-h-screen p-4 px-8 pt-24 pb-10 md:px-12 md:pt-28 md:pb-10">
         <Heading as="h1">Contact Me</Heading>
-        <SubHeading as="h2">
+        <SubHeading as="h2" className="mb-8">
           I am always open to new opportunities and collaborations. Feel free to
-          reach out to me via email or LinkedIn.
+          reach out to me using the form below or via email/LinkedIn.
         </SubHeading>
-        <ContactForm />
+        <TallyForm />
       </Container>
     </div>
   );
